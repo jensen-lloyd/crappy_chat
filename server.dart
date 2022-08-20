72d0cb372d69f15697d8aae71e12c35e2ca9d7fa
jensen-lloyd <jl.110@protonmail.com>
2022-08-21 00:22:45 +1000

    Copied server code from a blog lol

--- server.dart
+++ server.dart
@@ -1,11 +1,50 @@
-import 'package:dart_websockets/server.dart';
+//import 'package:dart_websockets/server.dart';
 import 'dart:io';
 
+const int port = 1000;
+List<WebSocket> connections;
 
-void main() async
+
+void main()
 {
 
-echo("test for github");
+    connections = new List<WebSocket>();
 
+    HttpServer.bind(InternetAddress.ANY_UP_V4, port).then((HttpServer server)
+    {
+        print('Server listening on port ${port}.');
+        server.listen((HttpRequest request)
+        {
+
+            if (WebsocketTransformer.isUpgradeRequest(request)))
+            {
+                WebSocketTransformer.upgrade(request).then(Websocket ws)
+                {
+                    connections.add(ws);
+                    print('Client connected, there are now ${connections.length} client(s) connected.');
+                    ws.listen((String message)
+                    {
+                        for (Websocket connection in connections)
+                        {
+                            connection.add(message);
+                        }
+                    },
+
+                    onDone: ()
+                    {
+                        connections.remove(ws);
+                        print('Client disconnected, there are now ${connections.length} client(s) connected.');
+                    });
+                });
+            }
+
+            else
+            {
+                request.response.statusCode = HttpStatus.FORBIDDEN;
+                request.response.reasonPhrase = 'Websocket connections only!';
+                request.response.close();
+            }
+        }
+    });
 
 }
