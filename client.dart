e3cf685d690dc2b3edd95fc294c32ce6003338ed
jensen-lloyd <jl.110@protonmail.com>
2022-08-21 22:29:22 +1000

    Working websocket connection.

--- client.dart
+++ client.dart
@@ -1,13 +1,15 @@
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
-
 import 'dart:io';
+
+
+String address = "127.0.0.1";
+int port = 1975;
+
 
 main() async
 {
     //var channel = IOWebSocketChannel.connect(Uri.parse('ws://thetechclicks.com:1975');
 
-    var socket = await WebSocket.connect('ws://thetechclicks.com:1975/ws');
+    var socket = await WebSocket.connect('ws://${address}:${port}/ws');
     socket.add('Hello, I am a client!');
 
 
--- server.dart
+++ server.dart
@@ -1,25 +1,82 @@
 import 'dart:io';
 
+
+int port = 1975;
+String address = '127.0.0.1'; 
+
+
 void  main() async
 {
-    var server = await HttpServer.bind('127.0.0.1', 1975);
-    //await server.forEach((HttpRequest request)
-    //{
-    //    request.response.write('Hello, world!');
-    //    request.response.close();
-    //});
-    
-
+    var server = await HttpServer.bind(address, port);
     server.listen((HttpRequest req) async
     {
+        print('Connection requested');
+        print(req);
+
         if (req.uri.path == '/ws')
         {
             var socket = await WebSocketTransformer.upgrade(req);
-            print(socket.listen());
+            print('Connection upgraded to WebSocket');
+            socket.listen((client) 
+            {
+                //handleConnection(client);
+                print(client);
+            });
         }
     });
 
 
 
     
+}
+
+
+
+void handleConnection(Socket client)
+{
+    print('Connection from ${client.remoteAddress.address}:${client.remotePort}');
+
+    // listen for events from the client
+    client.listen(
+
+    // handle data from the client
+    (data) async 
+    {
+        await Future.delayed(Duration(seconds: 1));
+        final message = String.fromCharCodes(data);
+     
+        if (message == '') 
+        {
+        client.write('Who is there?');
+        } 
+
+        else if (message.length < 10) 
+        {
+            client.write('$message who?');
+        } 
+
+        else 
+        {
+            client.write('Very funny.');
+            client.close();
+        }
+    },
+
+    // handle errors
+    onError: (error) 
+    {
+        print(error);
+        client.close();
+    },
+
+    // handle the client closing the connection
+    onDone: () 
+    {
+        print('Client left');
+        client.close();
+    },
+  );
+
+
+
 }
