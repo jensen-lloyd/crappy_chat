b83f994db5ebe65c1bf66f5f9fbc14301980f7b3
jensen-lloyd <jl.110@protonmail.com>
2022-08-23 22:10:48 +1000

    Minor changes to client (sends version number)

--- client.dart
+++ client.dart
@@ -3,19 +3,23 @@ import 'dart:io';
 
 String address = "127.0.0.1";
 int port = 1975;
+String version = "0.1";
 
+List<String> servers = ["127.0.0.1", "neostalgia.ddns.net", "thetechclicks.com"];
 
 main() async
 {
     //var channel = IOWebSocketChannel.connect(Uri.parse('ws://thetechclicks.com:1975');
 
     var socket = await WebSocket.connect('ws://${address}:${port}/ws');
-    socket.add('Hello ${address}, I am a client!');
+    socket.add(version);
     print("Connected to ${address} on port ${port}.");
 
     print("Enter a username for chat: ");
     String? username = stdin.readLineSync();
-    socket.add("My username is: ${username}");
+    socket.add(username);
+
+    
 
 
 }
